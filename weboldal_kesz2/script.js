924fcd04e36d0a9dc615e033bd06c5ed917168d8
Kissdani05 <70641401+Kissdani05@users.noreply.github.com>
2024-02-08 11:05:36 +0100

    Kész

---
+++ weboldal_kesz2/script.js
@@ -0,0 +1,383 @@
+var adatok = [
+  {
+    "OM_Azonosito": "78655218932",
+    "Neve": "Szabó Anna",
+    "ErtesitesiCime": "Budapest, Gellért tér 15.",
+    "Email": "anna@example.com",
+    "SzuletesiDatum": "1998-07-19T00:00:00",
+    "Matematika": 14,
+    "Magyar": 35
+  },
+  {
+    "OM_Azonosito": "15963702584",
+    "Neve": "Nagy Zsófia",
+    "ErtesitesiCime": "Debrecen, Szent István utca 8.",
+    "Email": "zsofia@example.com",
+    "SzuletesiDatum": "2000-02-22T00:00:00",
+    "Matematika": 27,
+    "Magyar": 4
+  },
+  {
+    "OM_Azonosito": "30351479261",
+    "Neve": "Kovács Máté",
+    "ErtesitesiCime": "Szeged, Erzsébet körút 45.",
+    "Email": "mate@example.com",
+    "SzuletesiDatum": "1995-11-29T00:00:00",
+    "Matematika": 48,
+    "Magyar": 15
+  },
+  {
+    "OM_Azonosito": "97401028543",
+    "Neve": "Tóth Bence",
+    "ErtesitesiCime": "Pécs, Váci utca 33.",
+    "Email": "bence@example.com",
+    "SzuletesiDatum": "1997-03-17T00:00:00",
+    "Matematika": 8,
+    "Magyar": 47
+  },
+  {
+    "OM_Azonosito": "88765031624",
+    "Neve": "Horváth Eszter",
+    "ErtesitesiCime": "Székesfehérvár, Bartók Béla út 12.",
+    "Email": "eszter@example.com",
+    "SzuletesiDatum": "1996-09-08T00:00:00",
+    "Matematika": 34,
+    "Magyar": 7
+  },
+  {
+    "OM_Azonosito": "64189075351",
+    "Neve": "Kiss Attila",
+    "ErtesitesiCime": "Miskolc, József Attila utca 18.",
+    "Email": "attila@example.com",
+    "SzuletesiDatum": "1993-12-05T00:00:00",
+    "Matematika": 13,
+    "Magyar": 48
+  },
+  {
+    "OM_Azonosito": "18734250658",
+    "Neve": "Fekete Laura",
+    "ErtesitesiCime": "Győr, Széchenyi tér 9.",
+    "Email": "laura@example.com",
+    "SzuletesiDatum": "1999-06-30T00:00:00",
+    "Matematika": 2,
+    "Magyar": 30
+  },
+  {
+    "OM_Azonosito": "51698072427",
+    "Neve": "Bíró Gábor",
+    "ErtesitesiCime": "Kecskemét, Deák Ferenc utca 21.",
+    "Email": "gabor@example.com",
+    "SzuletesiDatum": "1994-10-14T00:00:00",
+    "Matematika": 9,
+    "Magyar": 33
+  },
+  {
+    "OM_Azonosito": "60157349268",
+    "Neve": "Mészáros Péter",
+    "ErtesitesiCime": "Nyíregyháza, Petőfi Sándor utca 26.",
+    "Email": "peter@example.com",
+    "SzuletesiDatum": "2001-04-01T00:00:00",
+    "Matematika": 36,
+    "Magyar": 21
+  },
+  {
+    "OM_Azonosito": "72948316750",
+    "Neve": "Varga Noémi",
+    "ErtesitesiCime": "Szombathely, Kossuth Lajos utca 3.",
+    "Email": "noemi@example.com",
+    "SzuletesiDatum": "1992-08-18T00:00:00",
+    "Matematika": 24,
+    "Magyar": 23
+  },
+  {
+    "OM_Azonosito": "84052731649",
+    "Neve": "Lakatos Dóra",
+    "ErtesitesiCime": "Veszprém, Ady Endre utca 7.",
+    "Email": "dora@example.com",
+    "SzuletesiDatum": "2000-01-03T00:00:00",
+    "Matematika": 43,
+    "Magyar": 41
+  },
+  {
+    "OM_Azonosito": "85273941680",
+    "Neve": "Németh Tamás",
+    "ErtesitesiCime": "Szolnok, Béke tér 14.",
+    "Email": "tamas@example.com",
+    "SzuletesiDatum": "1998-05-27T00:00:00",
+    "Matematika": 5,
+    "Magyar": 49
+  },
+  {
+    "OM_Azonosito": "41593260701",
+    "Neve": "Orbán Katalin",
+    "ErtesitesiCime": "Eger, Szabadság utca 32.",
+    "Email": "katalin@example.com",
+    "SzuletesiDatum": "1996-02-11T00:00:00",
+    "Matematika": 37,
+    "Magyar": 20
+  },
+  {
+    "OM_Azonosito": "10486732952",
+    "Neve": "Simon Balázs",
+    "ErtesitesiCime": "Debrecen, Király utca 28.",
+    "Email": "balazs@example.com",
+    "SzuletesiDatum": "1995-07-07T00:00:00",
+    "Matematika": 20,
+    "Magyar": 48
+  },
+  {
+    "OM_Azonosito": "92740581643",
+    "Neve": "Papp Viktória",
+    "ErtesitesiCime": "Kaposvár, Alkotmány utca 5.",
+    "Email": "viktor@example.com",
+    "SzuletesiDatum": "1997-11-24T00:00:00",
+    "Matematika": 32,
+    "Magyar": 9
+  },
+  {
+    "OM_Azonosito": "10637851454",
+    "Neve": "Molnár Zoltán",
+    "ErtesitesiCime": "Szekszárd, Párizsi utca 17.",
+    "Email": "zoltan@example.com",
+    "SzuletesiDatum": "1993-01-16T00:00:00",
+    "Matematika": 3,
+    "Magyar": 46
+  },
+  {
+    "OM_Azonosito": "44025967885",
+    "Neve": "Fekete Márton",
+    "ErtesitesiCime": "Pécs, Rákóczi út 13.",
+    "Email": "marton@example.com",
+    "SzuletesiDatum": "1992-04-29T00:00:00",
+    "Matematika": 42,
+    "Magyar": 31
+  },
+  {
+    "OM_Azonosito": "30381425616",
+    "Neve": "Pál Júlia",
+    "ErtesitesiCime": "Sopron, Szent Gellért tér 10.",
+    "Email": "julia@example.com",
+    "SzuletesiDatum": "1999-09-02T00:00:00",
+    "Matematika": 49,
+    "Magyar": 19
+  },
+  {
+    "OM_Azonosito": "65082317920",
+    "Neve": "Takács Orsolya",
+    "ErtesitesiCime": "Eger, Andrássy út 22.",
+    "Email": "orsolya@example.com",
+    "SzuletesiDatum": "1994-06-13T00:00:00",
+    "Matematika": 31,
+    "Magyar": 18
+  },
+  {
+    "OM_Azonosito": "15374680221",
+    "Neve": "Kovács Ádám",
+    "ErtesitesiCime": "Székesfehérvár, Bajnai út 8.",
+    "Email": "adam@example.com",
+    "SzuletesiDatum": "1996-08-06T00:00:00",
+    "Matematika": 18,
+    "Magyar": 10
+  }
+];
+
+window.addEventListener('load', function() {
+  displayAllData();
+});
+
+document.getElementById('omAzon').addEventListener('input', function() {
+  const inputVal = this.value.replace(/[^0-9]/g, '');
+  this.value = inputVal;
+
+  const foundData = adatok.filter(adat => adat.OM_Azonosito.startsWith(inputVal));
+  displayResults(foundData);
+});
+
+function displayAllData() {
+  displayResults(adatok); // Display all data initially
+}
+
+function displayResults(data) {
+  var resultsContainer = document.getElementById('resultsContainer');
+  resultsContainer.innerHTML = ''; 
+
+  if (data.length === 0) {
+      resultsContainer.innerHTML = '<p>No results found</p>';
+      return;
+  }
+
+  data.forEach(function(item, index) {
+      var detailList = document.createElement('ul');
+      detailList.style.padding = '10px';
+      detailList.style.background = index % 2 === 0 ? '#f9f9f9' : '#ffffff'; 
+      detailList.style.borderRadius = '5px';
+      detailList.style.boxShadow = '0 2px 4px rgba(0,0,0,0.1)'; 
+      detailList.style.marginBottom = '10px'; 
+
+      Object.keys(item).forEach(function(key) {
+          var listItem = document.createElement('li');
+          listItem.innerHTML = `<strong>${key}:</strong> ${item[key]}`;
+          detailList.appendChild(listItem);
+      });
+
+      resultsContainer.appendChild(detailList);
+
+      if (index < data.length - 1) {
+          var separator = document.createElement('div');
+          separator.style.height = '2px';
+          separator.style.background = '#eee';
+          separator.style.margin = '10px 0'; 
+          resultsContainer.appendChild(separator);
+      }
+  });
+}
+
+/*
+window.addEventListener('load', function() {
+  const resultsDiv = document.getElementById('searchResults');
+  resultsDiv.innerHTML = ''; 
+
+  if (adatok.length > 0) {
+      let sumMatematika = 0, sumMagyar = 0;
+      adatok.forEach(data => {
+          sumMatematika += data.Matematika;
+          sumMagyar += data.Magyar;
+      });
+      const avgMatematika = sumMatematika / adatok.length;
+      const avgMagyar = sumMagyar / adatok.length;
+      const avgCombined = avgMatematika + avgMagyar;
+
+      let table = `<table class="table-style">
+          <tr><th colspan="7">Elemek száma: ${adatok.length}, Matek átlag: ${avgMatematika.toFixed(1)}, Magyar átlag: ${avgMagyar.toFixed(1)}, Össz átlag: ${avgCombined.toFixed(1)}</th></tr>
+          <tr><th>OM Azonosító</th><th>Neve</th><th>ErtesitesiCíme</th><th>Email</th><th>Születési Dátum</th><th>Matematika</th><th>Magyar</th></tr>`;
+
+      adatok.forEach(data => {
+          table += `<tr>
+              <td>${data.OM_Azonosito}</td>
+              <td>${data.Neve}</td>
+              <td>${data.ErtesitesiCime}</td>
+              <td>${data.Email}</td>
+              <td>${data.SzuletesiDatum.split('T')[0]}</td>
+              <td>${data.Matematika}</td>
+              <td>${data.Magyar}</td>
+          </tr>`;
+      });
+
+      table += `</table>`;
+      resultsDiv.innerHTML = table;
+  } else {
+      resultsDiv.innerHTML = `<p>No results found</p>`;
+  }
+});
+
+
+document.getElementById('omAzon').addEventListener('input', function() {
+  const inputVal = this.value.replace(/[^0-9]/g, '');
+  this.value = inputVal;
+
+  const foundData = adatok.filter(adat => adat.OM_Azonosito.startsWith(inputVal));
+
+  const resultsDiv = document.getElementById('searchResults');
+  resultsDiv.innerHTML = ''; 
+
+  if (foundData.length > 0) {
+      let sumMatematika = 0, sumMagyar = 0;
+      foundData.forEach(data => {
+          sumMatematika += data.Matematika;
+          sumMagyar += data.Magyar;
+      });
+      const avgMatematika = sumMatematika / foundData.length;
+      const avgMagyar = sumMagyar / foundData.length;
+      const avgCombined = avgMatematika + avgMagyar;
+
+      let table = `<table class="table-style">
+          <tr><th colspan="7">Elemek száma: ${foundData.length}, Matek átlag: ${avgMatematika.toFixed(2)}, Magyar átlag: ${avgMagyar.toFixed(2)}, Össz átlag: ${avgCombined.toFixed(2)}</th></tr>
+          <tr><th>OM Azonosító</th><th>Neve</th><th>ErtesitesiCíme</th><th>Email</th><th>Születési Dátum</th><th>Matematika</th><th>Magyar</th></tr>`;
+
+      foundData.forEach(data => {
+          table += `<tr>
+              <td>${data.OM_Azonosito}</td>
+              <td>${data.Neve}</td>
+              <td>${data.ErtesitesiCime}</td>
+              <td>${data.Email}</td>
+              <td>${data.SzuletesiDatum.split('T')[0]}</td>
+              <td>${data.Matematika}</td>
+              <td>${data.Magyar}</td>
+          </tr>`;
+      });
+
+      table += `</table>`;
+      resultsDiv.innerHTML = table;
+  } else {
+      resultsDiv.innerHTML = `<p>No results found</p>`;
+  }
+});
+*/
+
+
+/*
+document.getElementById('keresGomb').addEventListener('click', function() {
+  const inputVal = document.getElementById('omAzon').value;
+  const foundData = adatok.filter(adat => adat.OM_Azonosito.includes(inputVal));
+
+  if (foundData.length > 0) {
+      const newWindow = window.open('', '_blank', 'width=600,height=400,resizable,scrollbars');
+      newWindow.document.write(`
+          <style>
+              body {
+                  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+                  line-height: 1.6;
+                  color: #333;
+                  padding: 20px;
+                  background-color: #f4f4f4; 
+              }
+              h1 {
+                  color: #333;
+                  text-align: center;
+                  margin-bottom: 20px;
+              }
+              p {
+                  color: #333;
+                  font-size: 16px;
+                  background-color: #fff; 
+                  padding: 15px;
+                  border-radius: 10px; 
+                  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
+                  margin-bottom: 10px; 
+              }
+              hr {
+                border: 1px solid #28a745; 
+                height: 2px; 
+                background-color: #28a745; 
+                margin: 20px 0; 
+                border-radius: 20px;
+                opacity: 0.3;
+            }
+          </style>
+      `);
+      newWindow.document.write("<h1>Talált OM Azonosító adatai</h1>");
+      let sumMatematika = 0, sumMagyar = 0;
+      foundData.forEach(data => {
+        sumMatematika += data.Matematika;
+        sumMagyar += data.Magyar;
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                newWindow.document.write(`<p><b>${key}</b>: ${data[key]}</p>`);
+            }
+        }
+        newWindow.document.write('<hr>'); 
+    });
+    const avgMatematika = sumMatematika / foundData.length;
+    const avgMagyar = sumMagyar / foundData.length;
+    const avgCombined = (avgMatematika + avgMagyar) / 2;
+
+    // Displaying the summary statistics
+    newWindow.document.write(`<p><b>Listában lévő elemek száma</b>: ${foundData.length}</p>`);
+    newWindow.document.write(`<p><b>Matek pontok átlaga</b>: ${avgMatematika.toFixed(2)}</p>`);
+    newWindow.document.write(`<p><b>Magyar pontok átlaga</b>: ${avgMagyar.toFixed(2)}</p>`);
+    newWindow.document.write(`<p><b>Összpontszám átlaga</b>: ${avgCombined.toFixed(2)}</p>`);
+} else {
+      alert("Nincs ilyen OM Azonosító.\nEzt írtad be: " + inputVal + "\nPróbáld meg helyesen beírni.");
+  }
+});
+*/
